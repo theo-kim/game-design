2d5396c0ec51a4bbf43b2e0772c0bb194ea63b8d
Theodore Kim <tk1931@nyu.edu>
2019-10-22 12:40:06 -0400

    Removing ignored files

---
+++ include/global/LoadTexture.h
@@ -0,0 +1,28 @@
+#ifndef LOAD_TEXTURE_INCLUDE
+#define LOAD_TEXTURE_INCLUDE
+
+#define GL_SILENCE_DEPRECATION
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include <iostream>
+
+class TextureSheet {
+public:
+  TextureSheet();
+  TextureSheet(const char* filePath, int numRows, int numCols);
+  GLuint GetId();
+  float GetSize();
+  float GetSpriteSize();
+  void GetSprite (float *coords, int x, int y);
+private:
+  GLuint LoadTexture (const char* filePath);
+  GLuint id;
+  float sheetHeight;
+  float sheetWidth;
+  float spriteWidth;
+  float spriteHeight;
+};
+
+#endif
