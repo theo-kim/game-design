856016097399a7e4c43ef6f017c559ccdee9da5d
Jin Su <jinsu@10-18-190-251.dynapool.wireless.nyu.edu>
2019-12-10 20:50:18 -0500

    1.0 Network.cpp added BeginSocket() via static ip

--- final/include/NetworkConnection.h
+++ final/include/NetworkConnection.h
@@ -1,6 +1,20 @@
 #ifndef NETWORKCONN_DEF
 #define NETWORKCONN_DEF
 
+#include <stdio.h> 
+#include <string.h> //strlen 
+#include <vector>
+#include <iostream>
+#include <stdlib.h> 
+#include <errno.h> 
+#include <unistd.h> //close 
+#include <arpa/inet.h> //close 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <pthread.h>
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
+
 class NetworkConnection {
 public:
   void Initialize();
